f67a965552a7ef3382333cf12db8e0a3659dded4
Mapelujo <olamideiyanda18@gmail.com>
2024-01-17 23:33:12 +0100

    JSON Object Successfully consumed

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
 'use strict';
 console.log('Hello Mr Lansa');
+
+// Getting Html Elements
+
+
+
+
+// Getting Data from Json file
+const getCountries = async () => {
+    const response = await fetch('./data.json')
+    const data = await response.json();
+
+}
+
+getCountries();
