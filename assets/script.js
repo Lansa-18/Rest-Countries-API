89cc92385aab08147dc4a71dbc0e37d16c700332
Mapelujo <olamideiyanda18@gmail.com>
2024-01-18 00:11:44 +0100

    progress

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,31 @@
 console.log('Hello Mr Lansa');
 
 // Getting Html Elements
+const countriesContainer = document.querySelector('.countries-grid-container');
 
-
-
+// FUNCTIONS
 
 // Getting Data from Json file
 const getCountries = async () => {
-    const response = await fetch('./data.json')
-    const data = await response.json();
+  const response = await fetch('./data.json');
+  const datas = await response.json();
+  datas.forEach((data) => {
+    displayCountries(data);
+  })
+};
 
-}
-
+const displayCountries = data => {
+  const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <p class="country__row"><span>Population:</span>${data.population}</p>
+        <p class="country__row"><span>Region:</span>${data.region}</p>
+        <p class="country__row"><span>Capital:</span>${data.capital}</p>
+        </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+};
 getCountries();
